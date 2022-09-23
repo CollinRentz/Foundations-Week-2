0d1dd47480adc7884874dc2d68859ba2df3055e9
Collin Rentz <rentzcollin@gmail.com>
2022-09-22 22:31:18 -0600

    study for assessment

--- Day_3/functions-2/arrayMethods.js
+++ Day_3/functions-2/arrayMethods.js
@@ -17,8 +17,9 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
   (element, index, wholeArray)=>{}    Arrow Form
 */
 
-// CODE HERE
-const evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+
+const evenNumbers = mixedNumbers.filter((element, index, wholeArray)=>element % 2 === 0);
+console.log(evenNumbers);
 
 
 
@@ -38,9 +39,9 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
   (element, index, wholeArray)=>{}    Arrow Form
 */
 
-// CODE HERE
-const postTaxPrices // = prices.map(/* Provide Your Callback Here );
 
+const postTaxPrices = prices.map((element, index, array)=>element * 1.07);
+console.log(postTaxPrices);
 
 
 ////////// PROBLEM 3 //////////
@@ -56,10 +57,9 @@ const populations = [8175133, 3792621, 2695598, 2100263];
   (runningTotal, curElement, curIndex, wholeArray)=>{} Arrow Form
 */
 
-// CODE HERE
-const totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
 
-
+const totalPopulation = populations.reduce((runningTotal, curElement)=>runningTotal + curElement);
+console.log(totalPopulation);
 
 ////////// PROBLEM 4 //////////
 
@@ -81,9 +81,9 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
   Use the filter method to return only the monsters that have a CP of over 200.
 */
 
-// CODE HERE
-const myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
 
+const myStrongest = monstersInYourPocket.filter((element, index, array) => array[index].CP > 200);
+console.log(myStrongest);
 
 
 ////////// PROBLEM 5 //////////
@@ -99,8 +99,10 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
   Use a higher order method to get all the order totals after adding in the sales tax (given to you as a tax rate, hint: you'll need to do some multiplication). Your answer should be an array of numbers, one total for each order.
 */
 
-// CODE HERE
 
+
+const ordersTaxed = orders.map((element, index, array)=>array[index].price * (1 + array[index].tax));
+console.log(ordersTaxed);
 
 
 ////////// PROBLEM 6 //////////
@@ -119,4 +121,7 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method(s) to get the sum of bobsTotal.
 */
 
-// CODE HERE
+const bobsPurchases = purchases.filter((element, index, wholeArray)=> wholeArray[index].owner === 'Bob');
+console.log(bobsPurchases);
+const bobsTotal = bobsPurchases.reduce((previousValue, currentValue)=>previousValue + currentValue.price, 0);
+console.log(bobsTotal);
